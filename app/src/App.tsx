4285b3027052fd774bfaa0dfc8a89c287d06cc96
AMillsy <00adammills00@gmail.com>
2023-07-04 14:36:17 +0100

    using interfaces in articleComp

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ArticleComponent
-          title="This is a world"
-          content="Hi"
-        ></ArticleComponent>
+        <ArticleComponent title="This is a world"></ArticleComponent>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- app/src/components/articleComponent/articleComponent.tsx
+++ app/src/components/articleComponent/articleComponent.tsx
@@ -2,11 +2,16 @@ import "./articleComponent.module.css";
 
 interface ArticleData {
   title: string;
-  content: string;
+  content?: string;
 }
 
 function ArticleComponent(data: ArticleData) {
-  return <h2>{data.title}</h2>;
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      <p>Random things</p>
+    </div>
+  );
 }
 
 export default ArticleComponent;
